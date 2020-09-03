75dedfe3873de7856d7422b39b8dc8c7b13fa0d2
Ndeme <y.ndeme@sevendadvancedacademy.com>
2020-09-03 11:57:32 +0100

    push init

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,15 @@ const Fruits = require("../models/fruits");
 //espress router
 const router = express.Router();
 // get a list of fruits from the db
-router.get("/fruits", function (req, res, next) {
-  res.send({ type: "GET" });
+router.get("/fruits", async function (req, res, next) {
+    Fruits.find().then(function(result){
+        res.send(result);
+        console.log(result);
+        
+    })
+    // const result = await Fruits
+    // .find();
+    // res.send(result);
 });
 
 // add a new fruits to the db
@@ -16,20 +23,39 @@ router.get("/fruits", function (req, res, next) {
 //     res.status(200).send("Fruit save ");
 //   });
 // });
-router.post('/fruits', function(req, res, next){
-    Fruits.create(req.body).then(function(fruit){
-        res.send(fruit);
-    }).catch(next);
+router.post("/fruits", function (req, res, next) {
+  Fruits.create(req.body)
+    .then(function (fruit) {
+      res.send(fruit);
+    })
+    .catch(next);
 });
 
 // update a fruits in the db
-router.put("/fruits/:id", function (req, res, next) {
-  res.send({ type: "PUT" });
+router.put("/fruits/:id", async function (req, res, next) {
+  const result = await Fruits.findById(req.params.id);
+  if (!result) {
+    console.log("result : ", "No Document with this id");
+    res.send("Result : No Document with this id");
+    return;
+  } else {
+    Fruits.findByIdAndUpdate({ _id: req.params.id }, req.body)
+      .then(function (fruit) {
+        res.send(fruit);
+      })
+      .catch(next);
+  }
 });
 
 // delete a fruits from the db
-router.delete("/fruits/:id", function (req, res, next) {
-  res.send({ type: "DELETE" });
+router.delete("/fruits/:id", async function (req, res, next) {
+  Fruits.findByIdAndRemove({ _id: req.params.id })
+    .then(function (fruit) {
+      res.send(fruit);
+    })
+    .catch(next);
+  // const result = await Courses.delete({ _id: req.params.id });
+  // console.log("result :", result);
 });
 
 //export node module
